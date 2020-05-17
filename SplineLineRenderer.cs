cbeff874d15d3aa8482b99d0bad9cea2eddc33fc
Andrew Campbell <31598679+Fulby@users.noreply.github.com>
2020-05-17 11:23:52 +0100

    Add script to make a line renderer follow the spline path
    To use, add the Spline, LineRenderer and SplineLineRenderer to a gameobject. In play mode the line updated to match the spline each time the spline changes.

---
+++ SplineLineRenderer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineLineRenderer : MonoBehaviour
+{
+    public SplineMesh.Spline spline;
+    public LineRenderer line;
+
+    public float segmentLength = 1f;
+
+    void Start()
+    {
+        if(!spline)
+        {
+            spline = GetComponent<SplineMesh.Spline>();
+        }
+        if (!line)
+        {
+            line = GetComponent<LineRenderer>();
+        }
+        Debug.Assert(spline);
+        Debug.Assert(line);
+
+        spline.CurveChanged.AddListener(UpdateLineFromSpline);
+
+        UpdateLineFromSpline();
+    }
+
+    void UpdateLineFromSpline()
+    {
+        Debug.Assert(spline);
+        Debug.Assert(line);
+        
+        int zSegments = Mathf.FloorToInt(spline.Length / segmentLength);
+
+        Vector3[] positions = new Vector3[zSegments+1];
+        for(int i = 0; i < zSegments; i++)
+        {
+            positions[i] = GetPosition(i * segmentLength);
+        }
+
+        positions[zSegments] = GetPosition(spline.Length);
+
+        line.positionCount = zSegments + 1;
+        line.SetPositions(positions);
+    }
+
+    Vector3 GetPosition(float aLength)
+    {
+        if (line.useWorldSpace)
+        {
+            return transform.TransformPoint(spline.GetSampleAtDistance(aLength).location);
+        }
+        else
+        {
+            return spline.GetSampleAtDistance(aLength).location;
+        }
+    }
+}
